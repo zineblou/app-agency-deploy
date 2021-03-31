2341aa77b1355da226e9c9aaefd593dd936ed343
imane naaanani <imanenaa01@gmail.com>
2021-03-31 16:54:31 +0000

    FEAT-[DEV] - Commit the project socle.

---
+++ src/main/java/ma/atos/agencymanagement/AgencymanagementApplication.java
@@ -0,0 +1,13 @@
+package ma.atos.agencymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/atos/agencymanagement/AgencymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.atos.agencymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
