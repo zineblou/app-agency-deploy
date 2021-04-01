478b2182eae72205a87f5b98f4f71a763abbbfae
imane naaanani <imanenaa01@gmail.com>
2021-04-01 18:05:20 +0000

    FEAT-[DEV]-ADD CLASS HABILITATION

---
+++ src/main/java/ma/atos/agencymanagement/model/Habilitation.java
@@ -0,0 +1,31 @@
+package ma.atos.agencymanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+import java.util.Optional;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Habilitation {
+    @Id
+    private long id;
+    private String code;
+    //le libelé
+    private String name;
+    private Date startDate;
+    private Date endDate;
+
+
+
+}
+
